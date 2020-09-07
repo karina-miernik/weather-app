2d6e39f6d07d3116bb2807e09b2e41b73f725a8a
Karina Miernik <karina.miernik@gmail.com>
2020-09-07 11:35:13 +0200

    Updates

---
+++ precache-manifest.6916ff7ed0670a7fef6a0789492ed745.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e7b0830327d57e3a8dfcf4b710fc88",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "5870837e8c3bf2e7175c",
+    "url": "/weather-app/static/css/main.c1b73ddf.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/weather-app/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5870837e8c3bf2e7175c",
+    "url": "/weather-app/static/js/main.4d54240b.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "52f53d02bbe76c3c03bb37fbf2684ccf",
+    "url": "/weather-app/static/media/cold.52f53d02.jpg"
+  },
+  {
+    "revision": "34fbc0a1bd233a5b02521f2ebc30aa18",
+    "url": "/weather-app/static/media/hot.34fbc0a1.png"
+  },
+  {
+    "revision": "73532cca3579b847792a98776322de4b",
+    "url": "/weather-app/static/media/rain.73532cca.jpg"
+  },
+  {
+    "revision": "02966334ce2be107080fd5949bb6d71f",
+    "url": "/weather-app/static/media/snow.02966334.jpg"
+  },
+  {
+    "revision": "663742e5aa42ef6ac9b216340d96c8eb",
+    "url": "/weather-app/static/media/warm.663742e5.jpg"
+  }
+]);
